59f0732cb7a26d813129cdae90c50aae9bd497b1
Maik G <maik.g@outlook.com>
2026-05-31 19:01:25 +0200

    Refactor hero component by removing unused TypeAnimation and related elements

--- src/feature/components/hero_component.tsx
+++ src/feature/components/hero_component.tsx
@@ -1,4 +1,4 @@
-import { TypeAnimation } from 'react-type-animation'
+
 
 const hero_component = () => {
   return (
@@ -10,41 +10,6 @@ const hero_component = () => {
               Hey I´m maik and I am Front End Developer and I have a preference for dealing with problems.
             </p>
         </div>
-      <div className="mb-4 rounded-xl font-mono text-sm border-2 border-gray-500 w-140 overflow-hidden ">
-
-
-        <div className="bg-gray-400 px-4 py-3 flex gap-2">
-          <span className="w-3 h-3 rounded-full bg-red-500" />
-          <span className="w-3 h-3 rounded-full bg-yellow-500" />
-          <span className="w-3 h-3 rounded-full bg-green-500" />
-        </div>
-
-        <div className="bg-neutral-900 p-6 space-y-1 h-48 w-140">          
-            <p>
-            <TypeAnimation sequence={['const Dev = {', 1000]} speed={20} className="text-blue-400" cursor={false} />
-          </p>
-          <p className="pl-4">
-            <span className="text-gray-400">name: </span>
-            <TypeAnimation sequence={[1000, '"Maik Groth"', 1000]} speed={20} className="text-blue-300" cursor={false} />
-          </p>
-          <p className="pl-4">
-            <span className="text-gray-400">role: </span>
-            <TypeAnimation sequence={[2000, '"Frontend Developer"', 1000]} speed={20} className="text-blue-300" cursor={false} />
-          </p>
-          <p className="pl-4">
-            <span className="text-gray-400">location: </span>
-            <TypeAnimation sequence={[3500, '"Germany | Krummesse"', 1000]} speed={20} className="text-blue-300" cursor={false} />
-          </p>
-          <p className="pl-4">
-            <span className="text-gray-400">nextStep: </span>
-            <TypeAnimation sequence={[4500, '"Full-Stack Developer"', 1000]} speed={20} className="text-blue-300" cursor={false} />
-          </p>
-          <p className="pl-4">
-            <TypeAnimation sequence={[6000, '}', 1000]} speed={20} className="text-blue-400" cursor={false}/>
-          </p>
-        </div>
-
-      </div>          
     </div>
 </section>
   )
