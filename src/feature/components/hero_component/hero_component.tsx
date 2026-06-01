e272b3e250a5615e1c8e6480eb6015a663045377
Maik G <maik.g@outlook.com>
2026-06-01 20:25:17 +0200

    Add: Fonts and new style in hero component

--- src/feature/components/hero_component/hero_component.tsx
+++ src/feature/components/hero_component/hero_component.tsx
@@ -5,16 +5,15 @@ const hero_component = () => {
     <section className="min-h-screen flex items-center">
       <div className="grid px-10 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Front End Developer
+          <h1 className="font-Unbounded max-w-4xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            Front-End Developer
           </h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-            Hey I´m maik and I am Front End Developer and I have a preference
+          <p className="font-DM-Sans max-w-2xl mb-6 font-medium text-gray-500 lg:mb-8 md:text-xl lg:text-2xl">
+            Hey I´m Maik and I am Front-End Developer and I have a preference
             for dealing with problems.
           </p>
         </div>
         <div className="right">
-
         </div>
       </div>
     </section>
