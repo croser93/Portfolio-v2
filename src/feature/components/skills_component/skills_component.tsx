001fedc3941c35c06345eb7b320f9714f2b8ca4f
Maik G <maik.g@outlook.com>
2026-06-03 19:23:40 +0200

    Add: skills_component: enhance layout with section headers and improve skill item display

--- src/feature/components/skills_component/skills_component.tsx
+++ src/feature/components/skills_component/skills_component.tsx
@@ -1,8 +1,7 @@
 import './skills_component.css'
 import { useTranslation } from "react-i18next"
-import { _React, AngularIcon, ClaudeCode, Css3, Django, DockerIcon, Figma, FirebaseIcon, Github, GithubIcon, Javascript, N8nIcon, Python, SqliteIcon, Supabase, SupabaseIcon, TypescriptIcon } from "@dev.icons/react"
+import { _React, AngularIcon, ClaudeCode, Css3, DjangoIcon, DockerIcon, Figma, FirebaseIcon, GithubIcon, Javascript, N8nIcon, Python, Sass, SqliteIcon, SupabaseIcon, TailwindIcon, TypescriptIcon } from "@dev.icons/react"
 import { Html5 } from "@dev.icons/react/mono";
-
 
 type SkillItem = { name: string; Icon: React.ComponentType<{ size?: number }>; color?: string }
 
@@ -13,18 +12,19 @@ const Frontend: SkillItem[] = [
     { name: 'Angular', Icon: AngularIcon },
     { name: 'Typescript', Icon: TypescriptIcon },
     { name: 'React', Icon: _React },
+    { name: 'Tailwind', Icon: TailwindIcon },
+    { name: 'SASS', Icon: Sass },
 ]
-
 
 const Backend: SkillItem[] = [
     { name: 'Python', Icon: Python },
-    { name: 'Django ', Icon: Django  },
+    { name: 'Django ', Icon: DjangoIcon  },
 ]
 
 const Database: SkillItem[] = [
     { name: 'Supabase', Icon: SupabaseIcon },
     { name: 'Firebase ', Icon: FirebaseIcon  },
-    { name: 'Firebase ', Icon: SqliteIcon  },
+    { name: 'Sqlite ', Icon: SqliteIcon  },
 ]
 
 const Tools: SkillItem[] = [
@@ -42,38 +42,62 @@ const skills_component = () => {
         <section className="skills border-t-4 border-blue-500/20 px-24 py-18">
             <h2 className="text-4xl py-6 font-Unbounded font-medium text-white">{t('SKILLS.TITLE')}</h2>
             <div className="grid py-6 grid-cols-4 gap-14">
-                <div className="grid grid-cols-2 gap-4 bg-blue-100/10 border border-blue-500/40 py-18 rounded-4xl">
-                    {Frontend.map((item, index) => (
-                        <div key={index} className={`flex gap-4 items-center flex-col ${item.color ?? ''}`}>
-                            <item.Icon size={48} />
-                            <span className="text-white font-DM-Sans">{item.name}</span>
-                        </div>
-                    ))}
+                <div className='bg-blue-100/10 border border-blue-500/40 rounded-4xl flex-col py-6'>
+                    <div className='flex justify-center py-6'>
+                        <h2 className='text-2xl text-white font-Unbounded'>Front - End</h2>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8">
+                        {Frontend.map((item, index) => (
+                            <div key={index} className={`flex gap-2 items-center flex-col ${item.color ?? ''}`}>
+                                <item.Icon size={48} />
+                                <span className="text-white font-DM-Sans">{item.name}</span>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className="grid grid-cols-2 gap-4 bg-blue-100/10 border border-blue-500/40 py-18 rounded-4xl">
-                    {Backend.map((item, index) => (
-                        <div key={index} className="flex gap-4 items-center flex-col">
-                            <item.Icon size={48} />
-                            <span className="text-white">{item.name}</span>
-                        </div>
-                    ))}
+                    
+                <div className='bg-blue-100/10 border border-blue-500/40 rounded-4xl flex-col py-6'>
+                    <div className='flex justify-center py-6'>
+                        <h2 className='text-2xl text-white font-Unbounded'>Back - End</h2>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8">
+                        {Backend.map((item, index) => (
+                            <div key={index} className={`flex gap-2 items-center flex-col ${item.color ?? ''}`}>
+                                <item.Icon size={48} />
+                                <span className="text-white font-DM-Sans">{item.name}</span>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className="grid grid-cols-2 gap-4 bg-blue-100/10 border border-blue-500/40 py-18 rounded-4xl">
-                    {Database.map((item, index) => (
-                        <div key={index} className={`flex gap-4 items-center flex-col ${item.color ?? ''}`}>
-                            <item.Icon size={48} />
-                            <span className="text-white font-DM-Sans">{item.name}</span>
-                        </div>
-                    ))}
+
+                <div className='bg-blue-100/10 border border-blue-500/40 rounded-4xl flex-col py-6'>
+                    <div className='flex justify-center py-6'>
+                        <h2 className='text-2xl text-white font-Unbounded'>Database</h2>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8">
+                        {Database.map((item, index) => (
+                            <div key={index} className={`flex gap-2 items-center flex-col ${item.color ?? ''}`}>
+                                <item.Icon size={48} />
+                                <span className="text-white font-DM-Sans">{item.name}</span>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className="grid grid-cols-2 gap-4 bg-blue-100/10 border border-blue-500/40 py-18 rounded-4xl">
-                    {Tools.map((item, index) => (
-                        <div key={index} className="flex gap-4 items-center flex-col">
-                            <item.Icon size={48} />
-                            <span className="text-white">{item.name}</span>
-                        </div>
-                    ))}
+
+                <div className='bg-blue-100/10 border border-blue-500/40 rounded-4xl flex-col py-6'>
+                    <div className='flex justify-center py-6'>
+                        <h2 className='text-2xl text-white font-Unbounded'>Tools</h2>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8">
+                        {Tools.map((item, index) => (
+                            <div key={index} className={`flex gap-2 items-center flex-col ${item.color ?? ''}`}>
+                                <item.Icon size={48} />
+                                <span className="text-white font-DM-Sans">{item.name}</span>
+                            </div>
+                        ))}
+                    </div>
                 </div>
+
             </div>
         </section>
     )
