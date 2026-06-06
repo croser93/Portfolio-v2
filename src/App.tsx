e6189ed7f0a72b5cee158b48feafd43b0409e66f
Maik G <maik.g@outlook.com>
2026-06-06 10:58:36 +0200

    Add: Implement CommentsComponent and integrate it into App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HeroComponent from './feature/components/hero_component/hero_component.js
 import AboutMeComponent from './feature/components/about_me_component/about_me_component.js'
 import SkillsComponent from './feature/components/skills_component/skills_component.js'
 import ProjectsComponent from './feature/components/projects_component/projects_component.js'
+import CommentsComponent from './feature/components/comments_component/comments_component.js'
 
 function App() {
   return(
@@ -13,6 +14,7 @@ function App() {
     <AboutMeComponent />
     <SkillsComponent />
     <ProjectsComponent />
+    <CommentsComponent />
 
   </main>
   )
---
+++ src/feature/components/comments_component/comments_component.tsx
@@ -0,0 +1,9 @@
+
+
+const comments_component = () => {
+  return (
+    <div>comments-component</div>
+  )
+}
+
+export default comments_component
