469caa8e0a18cd2ba17d5bac2ffaa61f0b3ddc0b
Maik G <maik.g@outlook.com>
2026-06-11 12:03:12 +0200

    Add: Enhance footer component with navigation links and social icons; update styles for improved layout and animations

--- src/feature/components/projects_component/projects_Dialog.tsx
+++ src/feature/components/projects_component/projects_Dialog.tsx
@@ -1,6 +1,8 @@
 import './projects_component.css'
 import { type ProjectItem } from './projects_component'
 import { GithubIcon } from "@dev.icons/react"
+import { IconArrowBigRightLines , IconArrowBigLeftLines , IconX, IconArrowUpRight } from '@tabler/icons-react';
+
 
 
 const projects_Dialog = ({ onClose, project, onPrev, onNext }: {
@@ -12,7 +14,7 @@ const projects_Dialog = ({ onClose, project, onPrev, onNext }: {
         <section className='dialogbackdropt flex justify-center items-center'>
             <div className="dialogCard rounded-lg border border-white/10 bg-white/5 overflow-hidden shadow-lg">
                 <div className="justify-end flex p-4">
-                    <button className="closebtn" onClick={onClose}>x</button>
+                    <button className="closebtn" onClick={onClose}><IconX/></button>
                 </div>
                 <div className="py-4 items-center flex-col flex">
                     <img className="object-cover dialogimg" src={project.img}></img>
@@ -23,12 +25,12 @@ const projects_Dialog = ({ onClose, project, onPrev, onNext }: {
                     <div className="px-6 pt-2 pb-4 flex justify-end gap-3"></div>
                 </div>
                 <div className="flex justify-between p-4">
-                    <button className="closebtn"  onClick={onPrev}>◀</button>
+                    <button className="closebtn" onClick={onPrev}><IconArrowBigLeftLines/></button>
                     <div className="flex gap-4">
                         <a className="link flex justify-center items-center" href={project.github} target="_blank"><GithubIcon size={24} /></a>
-                        <a className="link flex justify-center items-center font-DM-Sans text-m" href={project.live} target="_blank">Live</a>
+                        <a className="link flex justify-center items-center font-DM-Sans text-m" href={project.live} target="_blank">Live <IconArrowUpRight/></a>
                     </div>
-                    <button className="closebtn" onClick={onNext}>▶</button>
+                    <button className="closebtn" onClick={onNext}><IconArrowBigRightLines/></button>
                 </div>
             </div>
         </section>
--- src/feature/shared/footer.tsx
+++ src/feature/shared/footer.tsx
@@ -1,7 +1,39 @@
+import {IconBrandGithub, IconBrandLinkedin, IconMail  } from '@tabler/icons-react';
+import { Link } from 'react-router-dom';
+import './navbar.css'
+import { useTranslation } from "react-i18next"
+import { HashLink } from 'react-router-hash-link';
+
+
 
 const footer = () => {
+    const { t } = useTranslation()
     return (
-    <div>footer</div>
+    <section className='footer border-t flex justify-between items-center px-16 py-4 bg-white/10'>
+        <div>
+        <HashLink to="/#home">
+            <img className="block dark:hidden w-10 h-10" src="/public/assets/svg/logo_dark.svg" alt="" />
+            <img className="hidden dark:block w-10 h-10" src="/public/assets/svg/logo_light.svg" alt="" />
+        </HashLink>
+
+        </div>
+        <div>
+            <div className='flex gap-5'>
+                <Link to="/legal-notice" className="block aUnderline py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"><span className='light:text-black font-DM-Sans font-medium'>{t('IMPRINT.IMPRINT')}</span></Link>
+                <Link to="/privacy-policy" className="block aUnderline py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"><span className='light:text-black font-DM-Sans font-medium'>{t('CONTACT.PRIVACY_POLICY')}</span></Link>
+
+            </div>
+
+        </div>
+        <div>
+            <div className="flex gap-4"> 
+                <a href="https://github.com/croser93" target='_blank'><div className='icon-github light:text-black hover:text-blue-400'><IconBrandGithub size={42}/></div></a>
+                <a href="https://in/maik-groth" target='_blank'><div className='icon-linkedin light:text-black hover:text-blue-400'><IconBrandLinkedin size={42}/></div></a>
+                <a href="https://in/maik-groth" target='_blank'><div className='icon-mail light:text-black hover:text-blue-400'><IconMail size={42}/></div></a>
+
+            </div>
+        </div>
+    </section>
     )
 }
 
